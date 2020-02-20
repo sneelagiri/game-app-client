e5e37fa5950ac71afdf6a1018268a1c2ebea6e64
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-20 16:51:31 +0100

    creating game state

--- src/components/Game/QuizGameContainer.js
+++ src/components/Game/QuizGameContainer.js
@@ -1,46 +1,74 @@
 import React, { Component } from "react";
-
+import Fade from "react-reveal/Fade";
+const axios = require("axios");
+const url = "http://localhost:4000";
 export default class QuizGameContainer extends Component {
+  state = {
+    username: "",
+    points: 0,
+    questions: [],
+    answeredCorrectly: [],
+    currentQuestion: 0,
+    gameover: false
+  };
+  stream = new EventSource(`${url}/stream`);
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      const { data } = event;
+      const action = JSON.parse(data);
+      console.log("What is the action?", action);
+    };
+  }
+  handleClick = option => {
+    this.state.questions.map(question => {
+      if (question.correct_answer === option) {
+        this.setState({ points: this.state.points + 10 });
+      }
+    });
+    if (this.state.currentQuestion < 9) {
+      this.setState({ currentQuestion: this.state.currentQuestion + 1 });
+    } else {
+      this.setState({ gameover: true, currentQuestion: 0 });
+    }
+  };
+
   render() {
+    const notLoading = this.state.questions.length !== 0;
+    const currQ = this.state.currentQuestion;
     return (
       <div>
-        <div id="header">Open Trivia!</div>
-        <div class="divider"></div>
-        <div id="content">
-          <div id="config" class="modifiers">
-            <form>
-              Questions:
-              <select id="noq" style="width:250px;">
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-              </select>
-            </form>
-            <form>
-              Category:
-              <select id="cat" style="width:250px;">
-                <option value="">Any</option>
-              </select>
-            </form>
-
-            <form>
-              Difficulty:
-              <select id="dif" style="width:250px;">
-                <option value="">Any</option>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-              </select>
-            </form>
-          </div>
-          <div id="start" class="option" onClick="setupGame()">
-            Start
-          </div>
-          <div id="playArea">
-            <div id="q"></div>
-            <div id="answers"></div>
-          </div>
-        </div>
+        {notLoading ? (
+          <Fade>
+            {this.state.gameover ? (
+              "Waiting for other players"
+            ) : (
+              <div>
+                <h2>{this.state.questions[currQ].question}</h2>
+                {this.state.questions[currQ].options.map(option => {
+                  return (
+                    <div>
+                      <br></br>
+                      <button
+                        name="option"
+                        value={option}
+                        onClick={() => {
+                          this.handleClick(option);
+                        }}
+                      >
+                        {option}
+                      </button>
+                      <br></br>
+                    </div>
+                  );
+                })}
+                <h3>Points: {this.state.points}</h3>
+              </div>
+            )}
+          </Fade>
+        ) : (
+          "Loading..."
+        )}
       </div>
     );
   }
